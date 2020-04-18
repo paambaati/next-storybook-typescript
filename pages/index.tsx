125e5521b669b1628da0beb993e2ea69e932537b
GP <exchequer598@gmail.com>
2020-04-18 13:36:25 +0530

    pages: Use the new global styles.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
+import Head from 'next/head';
 import Button from '../components/button';
+import globalStyles from '../styles/global';
 
-function HomePage() {
-    return <div>Welcome to Next.js!<Button/></div>
+const HomePage = () => {
+    return <>
+        <Head><style jsx global>
+            {globalStyles}
+        </style>
+        </Head>
+        <div>Welcome to Next.js!<Button /></div>
+    </>
 }
 
 export default HomePage;
